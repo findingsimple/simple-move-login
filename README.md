e7eb93c7ffddfc755873bad882c1e13d938d7af3
Jason Conroy <jason@findingsimple.com>
2013-07-24 19:38:42 +1000

    First commit of plugin/drop-in

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-simple-move-login
-=================
+# Simple Move Login #
+Contributors: jconroy  
+Tags: login   
+Requires at least: 3.5  
+Tested up to: 3.5  
+Stable tag: 1.0  
+
+Utility plugin (or drop-in) for changing the location of the login page (wp-login.php).  
+
+## Description ##
+
+A utility plugin (or can be used as a drop-in) for assisting with changing the location of the login page. For example changing `wp-login.php` to be `login.php`.
+
+Why do this? It assists with preventing brute force attacks that target the default wp-login.php page. 
+
+Please note this is more to help sysadmin and devs when there is a more servious brute force issue. You new login.php file will need to be manually updated whenever WP is updated.
+
+## Installation ##
+
+Installation is standard and straight forward. 
+
+1. Duplicate and rename wp-login.php to your desired "new" login file name e.g. `login.php` and find and replace all instances of `wp-login.php` with the new file name e.g. `login.php`. By default `login.php` is used by the plugin.
+2. Edit the your `wp-config.php` file and define the new login file name if `login.php` has not been used. e.g. ```/* Move wp-login.php */
+define('SML_NEW_LOGIN_PATH', 'login.php');```
+3. Upload `simple-move-login` folder (and all it's contents!) to the `/wp-content/plugins/` directory
+4. Activate the plugin through the 'Plugins' menu in WordPress
+
+For additional protection (and performance) the `wp-login.php` can be restricted by the web server. For example the following can be used in an nginx config to deny all access:
+```location ~ ^/(wp-login\.php) {
+        deny all;
+}```
+
+
+## Changelog ##
+
+### 1.0 ###
+* Initial release
---
+++ simple-move-login.php
@@ -0,0 +1,133 @@
+<?php
+/*
+ * Plugin Name: Simple Move Login
+ * Plugin URI: http://plugins.findingsimple.com
+ * Description: Helper plugin for moving/protecting wp-login.php against bruteforce attacks
+ * Version: 1.0
+ * Author: Finding Simple
+ * Author URI: http:www.findingsimple.com
+ * License: GPLv3
+ * Require: WordPress 3.5
+ * Text Domain: sml
+ */
+
+/**
+ * Filter site_url function to change wp-login.php
+ * 
+ * @param  [type] $url     [description]
+ * @param  [type] $path    [description]
+ * @param  [type] $scheme  [description]
+ * @param  [type] $blog_id [description]
+ * @return [type]          [description]
+ */
+function sml_site_url( $url, $path, $scheme, $blog_id = null ) {
+
+	if ( ($scheme === 'login' || $scheme === 'login_post') && !empty($path) && is_string($path) && strpos($path, '..') === false && strpos($path, 'wp-login.php') !== false ) {
+
+		// Base url
+		if ( empty( $blog_id ) || !is_multisite() ) {
+			$url = get_option( 'siteurl' );
+		} else {
+			switch_to_blog( $blog_id );
+			$url = get_option( 'siteurl' );
+			restore_current_blog();
+		}
+
+		$url = set_url_scheme( $url, $scheme );
+
+		return $url . sml_set_path( $path );
+	}
+
+	return $url;
+
+}
+
+add_filter( 'site_url', 'sml_site_url', 10, 4);
+
+
+/**
+ * Filter network_site_url function to change wp-login.php
+ * 
+ * @param  [type] $url    [description]
+ * @param  [type] $path   [description]
+ * @param  [type] $scheme [description]
+ * @return [type]         [description]
+ */
+function sml_network_site_url( $url, $path, $scheme ) {
+
+	if ( ($scheme === 'login' || $scheme === 'login_post') && !empty($path) && is_string($path) && strpos($path, '..') === false && strpos($path, 'wp-login.php') !== false ) {
+		global $current_site;
+
+		$url = set_url_scheme( 'http://' . $current_site->domain . $current_site->path, $scheme );
+
+		return $url . ltrim( sml_set_path( $path ) , '/' );
+
+	}
+
+	return $url;
+
+}
+
+add_filter( 'network_site_url', 'sml_network_site_url', 10, 3);
+
+
+
+/**
+ * Filter hardcoded wp-login.php redirects
+ * 
+ * @param  [type] $location [description]
+ * @param  [type] $status   [description]
+ * @return [type]           [description]
+ */
+function sml_redirect( $location, $status ) {
+
+	if ( site_url( reset( explode( '?', $location ) ) ) == site_url( 'wp-login.php' ) )
+		return sml_site_url( $location, $location, 'login', get_current_blog_id() );
+
+	return $location;
+
+}
+
+add_filter('wp_redirect', 'sml_redirect', 10, 2);
+
+
+
+/**
+ * Display error/message that the login form is no longer available at this location
+ * 
+ * @return [type] [description]
+ */
+function sml_login_init() {
+	
+	$uri = !empty($GLOBALS['HTTP_SERVER_VARS']['REQUEST_URI']) ? $GLOBALS['HTTP_SERVER_VARS']['REQUEST_URI'] : (!empty($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '');
+	
+	$uri = parse_url( $uri );
+	
+	$uri = !empty($uri['path']) ? str_replace( '/', '', basename($uri['path']) ) : '';
+
+	if ( $uri === 'wp-login.php' )
+		wp_die(__('No longer here.', 'sml'));
+
+}
+
+add_action( 'login_init', 'sml_login_init', 0 );
+
+
+/**
+ * Utility function for returning the new path / location of the login
+ * 
+ * @param  [type] $path [description]
+ * @return [type]       [description]
+ */
+function sml_set_path( $path ) {
+
+	if ( !defined( 'SML_NEW_LOGIN_PATH' ) )
+		$new_path = 'login.php'
+	else
+		$new_path = SML_NEW_LOGIN_PATH;
+
+	$path = str_replace('wp-login.php', $new_path , $path);
+
+	return '/' . ltrim( $path, '/' );
+
+}
